aa56948f73dadc1bdd150c7d532696f5d486dd1b
Harshit2180 <chopraharshit2180@gmail.com>
2025-09-04 16:11:46 +0530

    Setup files and app routes

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home'
+import Movies from './pages/Movies'
+import MovieDetails from './pages/MovieDetails'
+import SeatLayout from './pages/SeatLayout'
+import MyBookings from './pages/MyBookings'
+import Favourite from './pages/Favourite'
+import MainLayout from './layout/MainLayout'
+import NotFound from './pages/NotFound'
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/movies", element: <Movies /> },
+      { path: "/movies/:id", element: <MovieDetails /> },
+      { path: "/movies/:id/:date", element: <SeatLayout /> },
+      { path: "/my-bookings", element: <MyBookings /> },
+      { path: "/favourite", element: <Favourite /> }
+    ]
+  },
+  {
+    path: "*",
+    element: <NotFound />
+  }
+])
+
+const App = () => {
+  return (
+    <div>
+      <RouterProvider router={appRouter} />
+    </div>
+  )
+}
+
+export default App
